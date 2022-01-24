286a7bf46ff0a9495eff190b20751c9fc885b8c9
gemmiedodger <gemmadruce@live.co.uk>
2022-01-24 13:32:54 +0000

    Adding header component + initial styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
-
+import Header from "./components/Header";
+import './css/government_defaults.css';
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
