0543d4e15dfe99e12da2c460f4c27d71c4877902
gemmiedodger <gemmadruce@live.co.uk>
2022-01-24 13:35:32 +0000

    save of header (format update)

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,32 +5,51 @@ import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
-import logo from '../resources/logo/codemadeeasy_logo.png';
-import '../css/government_defaults.css';
+import logo from "../resources/logo/codemadeeasy_logo.png";
+import "../css/government_defaults.css";
 
 const Header = () => {
-  return(
-  <Navbar  expand="lg" className="pt-2 pb-2 background-darkest" data-testid="header">
-    <Container>
-      <a href="/" className="navbar-left"><img src={logo} className="img-responsive" width={250} alt="Code Made Easy Logo" /></a>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav className="me-auto">
-          <Nav.Link href="/" className="bg-transparent text-black">Home</Nav.Link>
-        </Nav>
-        <Nav className="me-auto">
-          <Nav.Link href="/" className="bg-transparent text-black">Learn</Nav.Link>
-        </Nav>
-        <Nav className="me-auto">
-          <Nav.Link href="/" className="bg-transparent text-black">Play</Nav.Link>
-        </Nav>
-        <Navbar.Text>
-          <Button className="bg-transparent" id="login-button">Login</Button>
-        </Navbar.Text>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
-}
+  return (
+    <Navbar
+      expand="lg"
+      className="pt-2 pb-2 background-darkest"
+      data-testid="header"
+    >
+      <Container>
+        <a href="/" className="navbar-left">
+          <img
+            src={logo}
+            className="img-responsive"
+            width={250}
+            alt="Code Made Easy Logo"
+          />
+        </a>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="me-auto">
+            <Nav.Link href="/" className="bg-transparent text-black">
+              Home
+            </Nav.Link>
+          </Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="/" className="bg-transparent text-black">
+              Learn
+            </Nav.Link>
+          </Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="/" className="bg-transparent text-black">
+              Play
+            </Nav.Link>
+          </Nav>
+          <Navbar.Text>
+            <Button className="bg-transparent" id="login-button">
+              Login
+            </Button>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
