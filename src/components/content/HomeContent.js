5e36c46b89cf2f327f207455023564d7e602bd6b
gemmiedodger <gemmadruce@live.co.uk>
2022-01-28 09:40:08 +0000

    fix for home content

--- src/components/content/HomeContent.js
+++ src/components/content/HomeContent.js
@@ -7,14 +7,14 @@ import Col from "react-bootstrap/Col";
 const HomeContent = (props) => {
   return (
     <Container className="h-100 mt-5 container">
-      <Row className="">
-        <Col className="">
+      <Row className="h-100">
+        <Col className="col-sm-12 my-auto align-middle mx-auto">
           {props.user && (
-            <h1 className="text-brightest pt-5 mt-5 text-center">Welcome home!</h1>
+            <h1 className="text-brightest mb-5 text-center">Welcome home!</h1>
           )}
-          <h3 className="text-center mt-5 pt-5 align-middle text-brightest ">
+          <h1 className="text-center align-middle text-brightest " >
             Your one-stop-shop to break into the world of programming.
-          </h3>
+          </h1>
         </Col>
       </Row>
     </Container>
